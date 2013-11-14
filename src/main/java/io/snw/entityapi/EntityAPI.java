a4ec0594f052eb98d2b587d6e43db0702550ce48
frostalf <frostalf@gmail.com>
2013-11-14 00:27:43 -0800

    Added Metrics

--- src/main/java/io/snw/entityapi/EntityAPI.java
+++ src/main/java/io/snw/entityapi/EntityAPI.java
@@ -1,6 +1,23 @@
 package io.snw.entityapi;
 
+import java.io.IOException;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
-public class EntityAPI extends JavaPlugin {
+public class EntityAPI extends JavaPlugin { 
+    
+
+    
+    @Override
+    public void onEnable(){
+        
+        try{
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+}
+    
 }
