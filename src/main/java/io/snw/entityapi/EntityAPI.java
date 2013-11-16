10a2396c82884c4eaa9109b47504eb51a30594bc
DSH105 <jason.chirgwin@ozemail.com.au>
2013-11-16 15:54:35 +1100

    You code, I'll format.

--- src/main/java/io/snw/entityapi/EntityAPI.java
+++ src/main/java/io/snw/entityapi/EntityAPI.java
@@ -1,35 +1,36 @@
 package io.snw.entityapi;
 
-import java.io.IOException;
 import org.bukkit.plugin.PluginBase;
 import org.mcstats.Metrics;
 
-public abstract class EntityAPI extends PluginBase { 
-    
+import java.io.IOException;
+
+public abstract class EntityAPI extends PluginBase {
+
     private static EntityAPI instance;
 
-    
-    public static Boolean hasInstance(){
+
+    public static Boolean hasInstance() {
         return instance != null;
     }
-    
-    public static EntityAPI getInstance(){
-        
-        if(instance == null){
+
+    public static EntityAPI getInstance() {
+
+        if (instance == null) {
             throw new RuntimeException("EntityAPI not Enabled, instance could not be found");
         }
         return instance;
     }
-    
+
     @Override
-    public void onEnable(){
-        
-        try{
+    public void onEnable() {
+
+        try {
             Metrics metrics = new Metrics(this);
             metrics.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
 }
