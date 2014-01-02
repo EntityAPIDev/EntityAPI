d7b34ff3f3547f7d74c775f81b79958e0d9cf406
frostalf <frostalf@gmail.com>
2014-01-02 05:29:39 -0800

    Adding Getting the Entity that is attacking

--- src/main/java/io/snw/entityapi/api/Attacking.java
+++ src/main/java/io/snw/entityapi/api/Attacking.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.LivingEntity;
 public interface Attacking {
 
     public LivingEntity getTarget();
+    
+    public LivingEntity getAttackingEntity();
 
     public void setTarget(LivingEntity target);
 }
