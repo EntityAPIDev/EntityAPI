8c30f919c18045074d9d3bcd87dcc5f32763ba7f
CaptainBern <captainberneby@gmail.com>
2014-01-02 11:26:18 +0100

    Added id + name to types.

--- src/main/java/io/snw/entityapi/api/ControllableEntityType.java
+++ src/main/java/io/snw/entityapi/api/ControllableEntityType.java
@@ -4,24 +4,37 @@ import io.snw.entityapi.entity.ControllableBaseEntity;
 import io.snw.entityapi.entity.ControllableEntityHandle;
 import io.snw.entityapi.entity.ControllableBat;
 import io.snw.entityapi.entity.ControllableBatEntity;
+import io.snw.entityapi.internal.Constants;
 
 public enum ControllableEntityType {
 
-    BAT(ControllableBat.class, ControllableBatEntity.class);
+    BAT(Constants.EntityTypes.Names.ENTITY_BAT, Constants.EntityTypes.Ids.ENTITY_BAT, ControllableBat.class, ControllableBatEntity.class);
 
-    Class<? extends ControllableBaseEntity> entityClass;
-    Class<? extends ControllableEntityHandle> handleClass;
+    private final String name;
+    private final int id;
+    private final Class<? extends ControllableBaseEntity> entityClass;
+    private final Class<? extends ControllableEntityHandle> handleClass;
 
-    ControllableEntityType(Class<? extends ControllableBaseEntity> entityClass, Class<? extends ControllableEntityHandle> handleClass) {
+    ControllableEntityType(String name, int id, Class<? extends ControllableBaseEntity> entityClass, Class<? extends ControllableEntityHandle> handleClass) {
+        this.name = name;
+        this.id = id;
         this.entityClass = entityClass;
         this.handleClass = handleClass;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
     public Class<? extends ControllableBaseEntity> getEntityClass() {
-        return entityClass;
+        return this.entityClass;
     }
 
     public Class<? extends ControllableEntityHandle> getHandleClass() {
-        return handleClass;
+        return this.handleClass;
     }
 }
