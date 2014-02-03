50789d51024702befb69bafc24104e76e1200cea
frostalf <frostalf@gmail.com>
2014-02-03 06:52:26 -0800

    Updated Chicking and Cow Entity classes.

--- src/main/java/io/snw/entityapi/entity/ControllableChicken.java
+++ src/main/java/io/snw/entityapi/entity/ControllableChicken.java
@@ -1,17 +1,32 @@
 package io.snw.entityapi.entity;
 
+import io.snw.entityapi.EntityManager;
 import io.snw.entityapi.api.ControllableEntityType;
 import io.snw.entityapi.api.EntitySound;
+import net.minecraft.server.v1_7_R1.EntityChicken;
 import org.bukkit.entity.Chicken;
 
 public class ControllableChicken extends ControllableBaseEntity<Chicken> {
 
-    public ControllableChicken(ControllableChickenEntity entityHandle) {
-        super(ControllableEntityType.CHICKEN);
+    public ControllableChicken(int id, EntityManager manager) {
+        super(id, ControllableEntityType.CHICKEN, manager);
+    }
+
+    public ControllableChicken(int id, ControllableBatEntity entityHandle, EntityManager manager) {
+        this(id, manager);
         this.handle = entityHandle;
         this.loot = entityHandle.getDefaultMaterialLoot();
     }
 
+    public void setHanging(boolean flag) {
+        ((EntityChicken) this.handle).a(flag);
+    }
+
+    public boolean isHanging() {
+        return ((EntityChicken) this.handle).bk();
+    }
+
+    @Override
     public void initSounds() {
         this.setSound(EntitySound.IDLE, "mob.chicken.idle");
         this.setSound(EntitySound.HURT, "mob.chicken.hurt");
--- src/main/java/io/snw/entityapi/entity/ControllableCow.java
+++ src/main/java/io/snw/entityapi/entity/ControllableCow.java
@@ -1,17 +1,32 @@
 package io.snw.entityapi.entity;
 
+import io.snw.entityapi.EntityManager;
 import io.snw.entityapi.api.ControllableEntityType;
 import io.snw.entityapi.api.EntitySound;
+import net.minecraft.server.v1_7_R1.EntityCow;
 import org.bukkit.entity.Cow;
 
 public class ControllableCow extends ControllableBaseEntity<Cow> {
 
-    public ControllableCow(ControllableCowEntity entityHandle) {
-        super(ControllableEntityType.COW);
+    public ControllableCow(int id, EntityManager manager) {
+        super(id, ControllableEntityType.CHICKEN, manager);
+    }
+
+    public ControllableCow(int id, ControllableBatEntity entityHandle, EntityManager manager) {
+        this(id, manager);
         this.handle = entityHandle;
         this.loot = entityHandle.getDefaultMaterialLoot();
     }
 
+    public void setHanging(boolean flag) {
+        ((EntityCow) this.handle).a(flag);
+    }
+
+    public boolean isHanging() {
+        return ((EntityCow) this.handle).bk();
+    }
+
+    @Override
     public void initSounds() {
         this.setSound(EntitySound.IDLE, "mob.cow.say");
         this.setSound(EntitySound.HURT, "mob.cow.hurt");
