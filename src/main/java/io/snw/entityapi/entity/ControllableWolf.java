cfd683fa94e9697b33fc04ecfbf4b11025069e9b
CaptainBern <captainberneby@gmail.com>
2014-02-04 11:45:26 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/java/io/snw/entityapi/entity/ControllableAttackingBaseEntity.java
    	src/main/java/io/snw/entityapi/entity/ControllableBlaze.java
    	src/main/java/io/snw/entityapi/entity/ControllableCaveSpider.java
    	src/main/java/io/snw/entityapi/entity/ControllableChicken.java
    	src/main/java/io/snw/entityapi/entity/ControllableCow.java
    	src/main/java/io/snw/entityapi/entity/ControllableCreeper.java
    	src/main/java/io/snw/entityapi/entity/ControllableEnderDragon.java
    	src/main/java/io/snw/entityapi/entity/ControllableEnderman.java
    	src/main/java/io/snw/entityapi/entity/ControllableGhast.java
    	src/main/java/io/snw/entityapi/entity/ControllableGiantZombie.java
    	src/main/java/io/snw/entityapi/entity/ControllableHorse.java
    	src/main/java/io/snw/entityapi/entity/ControllableIronGolem.java
    	src/main/java/io/snw/entityapi/entity/ControllableMagmaCube.java
    	src/main/java/io/snw/entityapi/entity/ControllableMushroomCow.java
    	src/main/java/io/snw/entityapi/entity/ControllableOcelot.java
    	src/main/java/io/snw/entityapi/entity/ControllablePig.java
    	src/main/java/io/snw/entityapi/entity/ControllablePigZombie.java
    	src/main/java/io/snw/entityapi/entity/ControllablePlayer.java
    	src/main/java/io/snw/entityapi/entity/ControllableSlime.java
    	src/main/java/io/snw/entityapi/entity/ControllableSpider.java
    	src/main/java/io/snw/entityapi/entity/ControllableSquid.java
    	src/main/java/io/snw/entityapi/entity/ControllableVillager.java
    	src/main/java/io/snw/entityapi/entity/ControllableWitch.java
    	src/main/java/io/snw/entityapi/entity/ControllableWither.java
    	src/main/java/io/snw/entityapi/entity/ControllableWolf.java
    	src/main/java/io/snw/entityapi/entity/ControllableZombie.java
    	src/main/java/io/snw/entityapi/utils/PlayerUtil.java

--- src/main/java/io/snw/entityapi/entity/ControllableWolf.java
+++ src/main/java/io/snw/entityapi/entity/ControllableWolf.java
@@ -1,13 +1,18 @@
 package io.snw.entityapi.entity;
 
+import io.snw.entityapi.EntityManager;
 import io.snw.entityapi.api.ControllableEntityType;
 import io.snw.entityapi.api.EntitySound;
 import org.bukkit.entity.Wolf;
 
 public class ControllableWolf extends ControllableBaseEntity<Wolf> {
 
-    public ControllableWolf(ControllableWolfEntity entityHandle) {
-        super(ControllableEntityType.WOLF);
+    public ControllableWolf(int id, EntityManager entityManager) {
+        super(id, ControllableEntityType.WOLF, entityManager);
+    }
+
+    public ControllableWolf(int id, ControllableWolfEntity entityHandle, EntityManager entityManager) {
+        this(id, entityManager);
         this.handle = entityHandle;
         this.loot = entityHandle.getDefaultMaterialLoot();
     }
