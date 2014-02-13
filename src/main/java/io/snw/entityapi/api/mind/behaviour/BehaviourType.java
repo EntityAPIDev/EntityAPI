346c3c40d89708d5af3b4c85347a992e3f258f2a
DSH105 <jason.chirgwin@icloud.com>
2014-02-13 21:08:36 +1100

    Temporary

--- src/main/java/io/snw/entityapi/api/mind/behaviour/BehaviourType.java
+++ src/main/java/io/snw/entityapi/api/mind/behaviour/BehaviourType.java
@@ -2,6 +2,15 @@ package io.snw.entityapi.api.mind.behaviour;
 
 public enum BehaviourType {
 
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIZE,
+    SEVEN;
+
     // NMS Goals have stored integers to check compatibility -> goal.j()
     // This enum is used to compare these goals easily and more friendly
 
