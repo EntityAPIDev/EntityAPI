031dd0e9bb131bb24fb1f9e6931193950bb1cdc4
DSH105 <jason.chirgwin@icloud.com>
2014-02-16 19:35:43 +1100

    Breed behaviour and events
    There is also another behaviour that will call these events. I’ll add
    it later.

---
+++ src/main/java/org/entityapi/api/events/ControllableEntityPreBreedEvent.java
@@ -0,0 +1,48 @@
+package org.entityapi.api.events;
+
+import net.minecraft.server.v1_7_R1.EntityAnimal;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.entityapi.api.ControllableEntity;
+
+public class ControllableEntityPreBreedEvent extends ControllableEntityEvent implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+
+    private Animals mate;
+    private Player breeder;
+
+    public ControllableEntityPreBreedEvent(ControllableEntity<Animals, EntityAnimal> firstParent, Animals mate, Player breeder) {
+        super(firstParent);
+        this.mate = mate;
+        this.breeder = breeder;
+    }
+
+    public Player getBreeder() {
+        return breeder;
+    }
+
+    public Animals getMate() {
+        return mate;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+}
