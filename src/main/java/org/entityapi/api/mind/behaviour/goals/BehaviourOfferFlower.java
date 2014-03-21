4867350743d40394f2e397dbe211f6a4746725b8
DSH105 <jason.chirgwin@icloud.com>
2014-03-22 07:49:10 +1100

    It compiles

--- src/main/java/org/entityapi/api/mind/behaviour/goals/BehaviourOfferFlower.java
+++ src/main/java/org/entityapi/api/mind/behaviour/goals/BehaviourOfferFlower.java
@@ -1,9 +1,14 @@
 package org.entityapi.api.mind.behaviour.goals;
 
+import org.entityapi.api.ControllableEntity;
 import org.entityapi.api.mind.behaviour.Behaviour;
 import org.entityapi.api.mind.behaviour.BehaviourType;
 
 public class BehaviourOfferFlower extends Behaviour {
+
+    public BehaviourOfferFlower(ControllableEntity controllableEntity) {
+        super(controllableEntity);
+    }
 
     @Override
     public BehaviourType getType() {
