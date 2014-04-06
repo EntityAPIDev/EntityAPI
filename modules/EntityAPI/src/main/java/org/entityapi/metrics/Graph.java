d862f387df4a19e733c076c191044fdf024b7a17
DSH105 <jason.chirgwin@icloud.com>
2014-04-06 10:44:30 +1000

    Fix copyright notices being duplicated

--- modules/EntityAPI/src/main/java/org/entityapi/metrics/Graph.java
+++ modules/EntityAPI/src/main/java/org/entityapi/metrics/Graph.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with EntityAPI.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.entityapi.metrics;
 
 import org.bukkit.plugin.Plugin;
